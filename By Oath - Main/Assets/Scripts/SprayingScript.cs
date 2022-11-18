4b9898a47928867cb335fb6c870e8e21d9c4cabd
Owen Baldwin <towlerowen@gmail.com>
2022-11-18 15:25:15 +1100

    Big splash

--- By Oath - Main/Assets/Scripts/SprayingScript.cs
+++ By Oath - Main/Assets/Scripts/SprayingScript.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class SprayingScript : MonoBehaviour
 {
     public ParticleSystem ScepterSpray;
+    public ParticleSystem BigScepterSpray;
+    public ParticleSystem BigSprayRings;
 
+    
+    public void Anim_SpecialSpray()
+    {
+        BigScepterSpray.Play();
+        BigSprayRings.Play();
+    }
+    
+    
     public void Anim_Spray()
     {
         ScepterSpray.Play();
