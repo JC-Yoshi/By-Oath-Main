8a129a5ebea6ba977de8801c1f3a694f16ab54f2
Lucas <liddyfelix.l@gmail.com>
2022-12-02 14:46:23 +1100

    Stuff idk

--- By Oath - Main/Assets/EndScreen.cs
+++ By Oath - Main/Assets/EndScreen.cs
@@ -8,6 +8,9 @@ public class EndScreen : MonoBehaviour
     void Start()
     {
         Time.timeScale = 1f;
+
+        Cursor.lockState = CursorLockMode.None;//locks the cursor to the center of the screen
+        Cursor.visible = true;
     }
 
     public void Restart()
